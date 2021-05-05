323a6d63ef2b97f272275abd221694d9266636c3
ushinnary <kartushin.alexander97@gmail.com>
2021-05-05 20:13:19 +0200

    Initial Commit

---
+++ Mac Toolbar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mac Toolbar
+//
+//  Created by Alexander Kartushin on 05/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Mac Toolbar/Mac_ToolbarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mac_ToolbarApp.swift
+//  Mac Toolbar
+//
+//  Created by Alexander Kartushin on 05/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct Mac_ToolbarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
